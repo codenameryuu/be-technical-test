4d614c210fddbb7ccb66ac6a2c36d3df898b18b8
codenameryuu <leaderballon@gmail.com>
2024-07-14 15:23:51 +0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import multipart from "@fastify/multipart";
+import { readFile } from "fs/promises";
+import { join } from "path";
 
 import { AppModule } from "./app.module";
 
@@ -16,6 +19,12 @@ async function bootstrap() {
       transform: true,
     })
   );
+
+  const options = new DocumentBuilder().setTitle("Title").setDescription("description").setVersion("1.0").build();
+
+  const document = JSON.parse((await readFile(join(process.cwd(), "src/docs/swagger.json"))).toString("utf-8"));
+
+  SwaggerModule.setup("/api-docs", app, document);
 
   await app.listen(env.app.port);
 }
